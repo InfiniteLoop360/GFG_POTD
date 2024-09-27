416b32fd9cb26b9022b466992af0c6e34ec5da38
InfiniteLoop360 <160904335+InfiniteLoop360@users.noreply.github.com>
2024-09-27 08:36:16 +0530

    K Sized Subarray Maximum

--- K Sized Subarray Maximum.java
+++ K Sized Subarray Maximum.java
@@ -1,3 +1,13 @@
+/*Approach:
+1. Use a deque: The idea is to maintain a sliding window of size k and keep track of the indices of the array elements in the deque.
+2. Maintain deque properties: 
+      Elements in the deque are in decreasing order. So the front of the deque will always contain the index of the maximum element of the current window.
+      Remove elements from the deque that are no longer in the current window.
+      For each new element, remove all elements in the deque that are smaller than the current element (as they can't be the maximum for any upcoming window).
+3. Sliding the window: As we slide the window from left to right, we maintain the deque so that the front of the deque always has the maximum element's index for the current window.
+4. Return result: At each step, add the element at the front of the deque to the result list as the maximum for that window.*/
+
+
 class Solution {
     // Function to find maximum of each subarray of size k.
     public ArrayList<Integer> max_of_subarrays(int k, int arr[]) {
