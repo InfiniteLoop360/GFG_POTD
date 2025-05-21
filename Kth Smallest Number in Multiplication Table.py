f7cfd807f8a58f5401f521652d894b109e30a6ea
InfiniteLoop360 <160904335+InfiniteLoop360@users.noreply.github.com>
2025-05-21 06:44:56 +0530

    Kth Smallest Number in Multiplication Table

---
+++ Kth Smallest Number in Multiplication Table.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def kthSmallest(self, m, n, k):
+        def countLessEqual(x):
+            # Counts how many numbers are <= x in the table
+            count = 0
+            for i in range(1, m + 1):
+                count += min(x // i, n)
+            return count
+
+        low, high = 1, m * n
+        while low < high:
+            mid = (low + high) // 2
+            if countLessEqual(mid) < k:
+                low = mid + 1
+            else:
+                high = mid
+        return low
