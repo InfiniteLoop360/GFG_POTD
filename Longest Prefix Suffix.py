0b3271ab0e44d5169576ac214ba46b8704403dc0
InfiniteLoop360 <160904335+InfiniteLoop360@users.noreply.github.com>
2025-08-08 07:18:50 +0530

    Longest Prefix Suffix

---
+++ Longest Prefix Suffix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def getLPSLength(self, s):
+        n = len(s)
+        lps = [0] * n  # Create LPS array of size n
+        length = 0     # length of previous longest prefix suffix
+
+        # Start from index 1 (index 0 is always 0)
+        i = 1
+        while i < n:
+            if s[i] == s[length]:
+                length += 1
+                lps[i] = length
+                i += 1
+            else:
+                if length != 0:
+                    length = lps[length - 1]
+                else:
+                    lps[i] = 0
+                    i += 1
+
+        return lps[-1]  # Last value is the answer
