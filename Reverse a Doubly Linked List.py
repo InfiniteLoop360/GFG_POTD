06f4790872705c6cf0503a95a540e7a6ee363122
InfiniteLoop360 <160904335+InfiniteLoop360@users.noreply.github.com>
2025-09-03 06:40:51 +0530

    Reverse a Doubly Linked List

---
+++ Reverse a Doubly Linked List.py
@@ -0,0 +1,29 @@
+"""
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+        self.prev = None
+"""
+
+class Solution:
+    def reverse(self, head):   # <-- changed from reverseDLL to reverse
+        # Edge case: empty or single node list
+        if head is None or head.next is None:
+            return head
+        
+        curr = head
+        new_head = None
+        
+        # Traverse and swap
+        while curr:
+            # Swap next and prev pointers
+            curr.prev, curr.next = curr.next, curr.prev
+            
+            # Update new_head to the last processed node
+            new_head = curr
+            
+            # Move to the "previous" node (which was next before swap)
+            curr = curr.prev  
+        
+        return new_head
