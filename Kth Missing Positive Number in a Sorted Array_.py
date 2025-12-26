39967df3713ee370bd3b51e7c3a43b8b5856cd10
InfiniteLoop360 <160904335+InfiniteLoop360@users.noreply.github.com>
2025-12-26 11:31:11 +0530

    Kth Missing Positive Number in a Sorted Array
    Implement binary search to find the kth missing positive number in a sorted array.

---
+++ Kth Missing Positive Number in a Sorted Array_.py
@@ -0,0 +1,15 @@
+class Solution:
+    def kthMissing(self, arr, k):
+        n = len(arr)
+        low, high = 0, n - 1
+        
+        while low <= high:
+            mid = (low + high) // 2
+            missing = arr[mid] - (mid + 1)
+            
+            if missing < k:
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return low + k
